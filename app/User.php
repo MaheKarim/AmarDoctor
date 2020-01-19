7b5eb421bc10d73f6ed6ca36b54382b5a3cdee86
mahekarim <mahe15-7770@diu.edu.bd>
2020-01-19 23:53:42 +0600

    LaraDash Package Add

--- app/User.php
+++ app/User.php
@@ -6,7 +6,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class
+User extends Authenticatable
 {
     use Notifiable;
 
@@ -17,9 +18,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'email', 'password',
-    ];
 
+    ];
     /**
+
      * The attributes that should be hidden for arrays.
      *
      * @var array
