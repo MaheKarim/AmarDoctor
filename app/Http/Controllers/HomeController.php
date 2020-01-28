3789074dfb0e547e7a9245b10b7bdebf1cc71f34
mahekarim <mahe15-7770@diu.edu.bd>
2020-01-29 00:10:04 +0600

    home building

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,9 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+    
     public function index()
     {
-        return view('backend.home');
+        return view('backend._home');
     }
 }
