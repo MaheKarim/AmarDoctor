aef7020496bc3e175b6eba616c2f8c606eea6857
mahekarim <mahe15-7770@diu.edu.bd>
2020-01-30 00:35:44 +0600

    logout feature

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+// Logout Route
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Route::get('/home', 'HomeController@index')->name('home');
