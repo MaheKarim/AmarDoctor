f36390f905db1a54c39b0ac4c15cbc412a400d28
mahekarim <mahe15-7770@diu.edu.bd>
2020-01-31 07:37:45 +0600

    product db add

---
+++ database/migrations/2020_01_31_012922_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('product_name');
+            $table->longText('description');
+            $table->string('total_rate');
+            $table->string('package_rate');
+            $table->string('package_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
