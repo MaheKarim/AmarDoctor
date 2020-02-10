988fd90e63a23f9dfcd89f552534d0a86a3ffe42
MaheKarim <mahekarim@gmail.com>
2020-02-10 14:52:27 +0600

    Search Page

--- resources/views/frontend/searchpage.blade.php
+++ resources/views/frontend/searchpage.blade.php
@@ -1,3 +1,167 @@
-@include('custom.header')
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!-- Basic Page Needs
+    ================================================== -->
+    <title>Hireo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="{{asset('/')}}search/css/style.css">
+    <link rel="stylesheet" href="{{asset('/')}}search/css/colors/blue.css">
+
+</head>
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper" class="wrapper-with-transparent-header">
+
+    <!-- Header Container
+    ================================================== -->
+    <header id="header-container" class="fullwidth transparent-header">
+
+        <!-- Header -->
+        <div id="header">
+            <div class="container">
+
+                <!-- Left Side Content -->
+                <div class="left-side">
+
+                    <!-- Logo -->
 
 
+                    <!-- Main Navigation -->
+
+                    <div class="clearfix"></div>
+                    <!-- Main Navigation / End -->
+
+                </div>
+                <!-- Left Side Content / End -->
+
+
+                <!-- Right Side Content / End -->
+
+                <!-- Right Side Content / End -->
+
+            </div>
+        </div>
+        <!-- Header / End -->
+
+    </header>
+    <div class="clearfix"></div>
+    <!-- Header Container / End -->
+
+
+
+    <!-- Intro Banner
+    ================================================== -->
+    <div class="intro-banner dark-overlay" data-background-image="{{asset('/')}}search/images/home-background-02.jpg">
+
+        <!-- Transparent Header Spacer -->
+        <div class="transparent-header-spacer"></div>
+
+        <div class="container">
+
+            <!-- Intro Headline -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="banner-headline">
+                        <h3>
+                            <strong>Hire experts Doctors for any job, any time.</strong>
+                            <br>
+                            <span>Huge community  for your Health.</span>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Search Bar -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="intro-banner-search-form margin-top-95">
+
+                        <!-- Search Field -->
+
+
+                        <!-- Search Field -->
+                        <div class="intro-search-field">
+                            <select class="selectpicker default" title="Select Any Category" name="ärea_name_id">
+                                @php($categories = \App\Category::all())
+                                @foreach($categories as $category)
+                                    <option value="{{$category->id}}">{{$category->category_name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <!-- Search Field -->
+                        <div class="intro-search-field">
+                            <select class="selectpicker default" title="All Categories" >
+                                <option>Admin Support</option>
+                                <option>Customer Service</option>
+                                <option>Data Analytics</option>
+                                <option>Design & Creative</option>
+                                <option>Legal</option>
+                                <option>Software Developing</option>
+                                <option>IT & Networking</option>
+                                <option>Writing</option>
+                                <option>Translation</option>
+                                <option>Sales & Marketing</option>
+                            </select>
+                        </div>
+
+                        <!-- Button -->
+                        <div class="intro-search-button">
+                            <button class="button ripple-effect" onclick="window.location.href='freelancers-grid-layout-full-page.html'">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Stats -->
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="intro-stats margin-top-45 hide-under-992px">
+                        <li>
+                            <strong class="counter">1,586</strong>
+                            <span>Doctor</span>
+                        </li>
+                        <li>
+                            <strong class="counter">3,543</strong>
+                            <span>Patient</span>
+                        </li>
+                        <li>
+                            <strong class="counter">1,232</strong>
+                            <span>Nurse</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+</div>
+<!-- Wrapper / End -->
+
+
+<!-- Scripts
+================================================== -->
+<script src="{{asset('/')}}search/js/jquery-3.4.1.min.js"></script>
+
+<script src="{{asset('/')}}search/js/mmenu.min.js"></script>
+<script src="{{asset('/')}}search/js/counterup.min.js"></script>
+<script src="{{asset('/')}}search/js/tippy.all.min.js"></script>
+<script src="{{asset('/')}}search/js/bootstrap-slider.min.js"></script>
+<script src="{{asset('/')}}search/js/bootstrap-select.min.js"></script>
+<script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
