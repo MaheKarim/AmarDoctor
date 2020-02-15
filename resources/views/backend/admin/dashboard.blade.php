ab270a8e2077e9e7c7ff9dd5333471d7ec6e3f7d
mahekarim <mahe15-7770@diu.edu.bd>
2020-02-16 04:09:19 +0600

    Admin ui/ux Change

--- resources/views/backend/admin/dashboard.blade.php
+++ resources/views/backend/admin/dashboard.blade.php
@@ -19,52 +19,43 @@
 </div>
 <div class="page-inner mt--5">
     <div class="row mt--2">
-        <div class="col-md-6">
-            <div class="card full-height">
-                <div class="card-body">
-                    <div class="card-title">Overall statistics</div>
-                    <div class="card-category">Daily information</div>
-                    <div class="d-flex flex-wrap justify-content-around pb-2 pt-4">
-                        <div class="px-2 pb-2 pb-md-0 text-center">
-                            <div id="circles-1"></div>
-                            <h6 class="fw-bold mt-3 mb-0">New Users</h6>
-                        </div>
-                        <div class="px-2 pb-2 pb-md-0 text-center">
-                            <div id="circles-2"></div>
-                            <h6 class="fw-bold mt-3 mb-0">Sales</h6>
-                        </div>
-                        <div class="px-2 pb-2 pb-md-0 text-center">
-                            <div id="circles-3"></div>
-                            <h6 class="fw-bold mt-3 mb-0">Subscribers</h6>
-                        </div>
+
+   <!--  End Star -->
+
+        <div class="col-md-4">
+            <div class="card card-secondary">
+                <div class="card-body skew-shadow">
+                    <h1>{{ \App\Booking::count() }}</h1>
+                    <h5 class="op-8">Total Call For Doctor Booking</h5>
+                    <div class="pull-right">
+                        <h3 class="fw-bold op-8">Booking</h3>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="card full-height">
-                <div class="card-body">
-                    <div class="card-title">Total income & spend statistics</div>
-                    <div class="row py-3">
-                        <div class="col-md-4 d-flex flex-column justify-content-around">
-                            <div>
-                                <h6 class="fw-bold text-uppercase text-success op-8">Total Income</h6>
-                                <h3 class="fw-bold">$9.782</h3>
-                            </div>
-                            <div>
-                                <h6 class="fw-bold text-uppercase text-danger op-8">Total Spend</h6>
-                                <h3 class="fw-bold">$1,248</h3>
-                            </div>
-                        </div>
-                        <div class="col-md-8">
-                            <div id="chart-container">
-                                <canvas id="totalIncomeChart"></canvas>
-                            </div>
-                        </div>
+        <div class="col-md-4">
+            <div class="card card-dark bg-secondary-gradient">
+                <div class="card-body bubble-shadow">
+                    <h1>{{ $users = \App\User::where('role_id','2')->count() }}</h1>
+                    <h5 class="op-8">Total Doctor</h5>
+                    <div class="pull-right">
+                        <h3 class="fw-bold op-8">Doctor Model</h3>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="card card-dark bg-secondary2">
+                <div class="card-body curves-shadow">
+                    <h1>{{ $users = \App\User::where('role_id','3')->count() }}</h1>
+                    <h5 class="op-8">Total Nurse</h5>
+                    <div class="pull-right">
+                        <h3 class="fw-bold op-8">Nurse Model</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 <!--   Data Table Add -->
     <div class="row">
