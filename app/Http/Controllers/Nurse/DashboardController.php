601c79dc972b757c53ab1c2ee4a5280b34b2ba5f
MaheKarim <mahekarim@gmail.com>
2020-02-17 15:02:32 +0600

    Nurse Settings

--- app/Http/Controllers/Nurse/DashboardController.php
+++ app/Http/Controllers/Nurse/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Nurse;
 
 
+use App\SiteSettings;
 use App\User;
 use Auth;
 use Illuminate\Http\Request;
@@ -14,7 +15,7 @@ class DashboardController extends Controller
     public function index()
     {
         $data = User::find(Auth::id());
-
-       return view('backend.multi-dashboard.nurse._home_nurse', compact('data'));
+        $settings = SiteSettings::find(1);
+       return view('backend.multi-dashboard.nurse._home_nurse', compact('data', 'settings'));
     }
 }
