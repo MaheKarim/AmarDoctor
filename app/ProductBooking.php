c06dd0664f885d0e08335f78191da557c132f667
MaheKarim <mahekarim@gmail.com>
2020-02-21 07:14:54 +0600

    ProductBooking Add

---
+++ app/ProductBooking.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductBooking extends Model
+{
+    // Code Start Here
+    protected $guarded = [ ];
+}
---
+++ database/migrations/2020_02_21_010456_create_product_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
+            $table->integer('product_id');
+            $table->integer('status_name_id')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_bookings');
+    }
+}
