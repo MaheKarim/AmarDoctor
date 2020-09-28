cf94ea3706800671217c6ab08a38ab3395235e0a
MaheKarim <mahekarim@gmail.com>
2020-09-28 11:24:04 +0600

    Previous File Deleted

--- resources/views/backend/multi-dashboard/doctor/_home_doctor.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('backend.multi-dashboard.doctor._layout_doctor')
-
-@section('content')
-
-
-@endsection
--- resources/views/backend/multi-dashboard/nurse/_home_nurse.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('backend.multi-dashboard.nurse._layout_nurse')
-
-@section('content')
-
-
-
-@endsection
