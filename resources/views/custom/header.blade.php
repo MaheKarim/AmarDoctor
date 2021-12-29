847a508c555e63aee1a52351af8d8a1435de4cdb
Mahe Karim <mahekarim@deligram.com>
2021-12-30 00:45:49 +0600

    Image Deleted - Logo Remove

--- resources/views/custom/header.blade.php
+++ resources/views/custom/header.blade.php
@@ -47,14 +47,16 @@
 							</span>
 						</a>
 						<a href="{{ url('/') }}" class="navbar-brand logo">
-							<img src="{{ asset('/') }}frontend/assets/img/logo.png" class="img-fluid" alt="Logo">
-						</a>
+{{--							<img src="{{ asset('/') }}frontend/assets/img/logo.png" class="img-fluid" alt="Logo">--}}
+                           <b>e-Health Care </b>
+                        </a>
 					</div>
 					<div class="main-menu-wrapper">
 						<div class="menu-header">
 							<a href="{{ url('/') }}" class="menu-logo">
-								<img src="{{ asset('/') }}frontend/assets/img/logo.png" class="img-fluid" alt="Logo">
-							</a>
+{{--								<img src="{{ asset('/') }}frontend/assets/img/logo.png" class="img-fluid" alt="Logo">--}}
+                                <b>e-Health Care </b>
+    							</a>
 							<a id="menu_close" class="menu-close" href="javascript:void(0);">
 								<i class="fas fa-times"></i>
 							</a>
